8883a368e204e8914659158211516f8c99554e29
amassiro <massironi.andrea@gmail.com>
2021-02-25 18:44:37 +0100

    KS tests

--- KS/test1.cxx
+++ KS/test1.cxx
@@ -32,7 +32,7 @@ void test1(int reference = 10000, int test = 1000) {
   float principal_delta = histo_reference->KolmogorovTest(histo_test, "M");
 
   //
-  // now check the pdf of "delta" to see if the option "X" is ok, even with more toys
+  // now check the pdf of "delta" to see if the option "X" is ok, even with more toys, and with the true pdf, the gauss
   //
   TCanvas* cc = new TCanvas ("cc", "", 800, 600);
   TH1F* histo_delta = new TH1F ("histo_delta", "", 300, 0, 1.0);
