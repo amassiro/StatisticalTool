08d841d1d0a8c20f8d64387263b2e4080c92d473
amassiro <massironi.andrea@gmail.com>
2026-07-07 16:53:19 +0200

    test

---
+++ AverageResults/README.md
@@ -0,0 +1,19 @@
+Test of the expected performance of averaged results
+
+
+    A +/- sigmaA
+    B +/- sigmaB
+
+    C = A+B
+
+
+Result:
+
+    A = 0.646 +/- 0.056    (8.66873 %)
+    B = 0.389 +/- 0.047    (12.0823 %)
+    C = 1.035 +/- 0.0731095    (7.06372 %)
+
+    A extimated = 0.646 +/- 0.0614092    (9.50607 %) stat unc = 1.64466 %
+    B extimated = 0.389 +/- 0.0421625    (10.8387 %) stat unc = 2.25138 %
+    C extimated = 1.035 +/- 0.102487    (9.90213 %)
+
---
+++ AverageResults/test.cxx
@@ -0,0 +1,82 @@
+
+
+void test(){
+
+  // cross sections
+  float A = 0.646;
+  float sigmaA = 0.056;
+
+  float B = 0.389;
+  float sigmaB = 0.047;
+
+  float C = A+B;
+  float sigmaC = sqrt(sigmaA*sigmaA + sigmaB*sigmaB);
+
+
+  std::cout << "A = " << A << " +/- " << sigmaA << "    (" << sigmaA/A*100 << " %)" << std::endl;
+  std::cout << "B = " << B << " +/- " << sigmaB << "    (" << sigmaB/B*100 << " %)" << std::endl;
+  std::cout << "C = " << C << " +/- " << sigmaC << "    (" << sigmaC/C*100 << " %)" << std::endl;
+
+  std::cout << " ------- " << std::endl;
+
+
+  //
+  // 10000 is the luminosity
+  //
+  float lumi = 10000;
+
+  float background_uncertainty = 0.09; // relative uncertainty
+
+  // signal A
+  float SigA = 7543;
+  float BkgA = 7847;
+  float effA = SigA / (A * lumi);
+
+  // signal B
+  float SigB = 4297;
+  float BkgB = 5062;
+  float effB = SigB / (B * lumi);
+
+  // signal C
+  float SigC = SigA + SigB;
+  float BkgC = BkgA + BkgB;
+  float effC = SigC / (C * lumi);
+
+
+  //
+  // in the estimation of A and B, I have "data - background",
+  // thus I have the statistical uncertainty of data, namely sqrt(data) = sqrt (signal + background)
+  // and the systematic uncertainty on B, background_uncertainty * Bkg
+  //
+
+  float C_extimated      = SigC/effC/lumi;
+  float sigmaC_extimated = std::hypot(sqrt(SigC+BkgC) , background_uncertainty * BkgC)/effC/lumi;
+
+  float A_extimated      = SigA/effA/lumi;
+  float sigmaA_extimated = std::hypot(sqrt(SigA+BkgA) , background_uncertainty * BkgA)/effA/lumi;
+  float stat_sigmaA_extimated = std::hypot(sqrt(SigA+BkgA) , 0. * BkgA)/effA/lumi;
+
+  float B_extimated      = SigB/effB/lumi;
+  float sigmaB_extimated = std::hypot(sqrt(SigB+BkgB) , background_uncertainty * BkgB)/effB/lumi;
+  float stat_sigmaB_extimated = std::hypot(sqrt(SigB+BkgB) , 0. * BkgB)/effB/lumi;
+
+
+  std::cout << "A extimated = " << A_extimated << " +/- " << sigmaA_extimated << "    (" << sigmaA_extimated/A_extimated*100 << " %)" << " stat unc = " << stat_sigmaA_extimated/A_extimated*100  << " % " << std::endl;
+  std::cout << "B extimated = " << B_extimated << " +/- " << sigmaB_extimated << "    (" << sigmaB_extimated/B_extimated*100 << " %)" << " stat unc = " << stat_sigmaB_extimated/B_extimated*100  << " % " << std::endl;
+  std::cout << "C extimated = " << C_extimated << " +/- " << sigmaC_extimated << "    (" << sigmaC_extimated/C_extimated*100 << " %)" << std::endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
